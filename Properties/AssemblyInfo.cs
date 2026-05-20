4945fead3e74a975e88d3fa4ff678d1f99aa09f9
Nir Zonshine <nirzon@gmail.com>
2026-05-20 07:09:16 +0300

    Clean the repo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -31,18 +31,18 @@ using System.Runtime.InteropServices;
 // The url to the license
 [assembly: AssemblyMetadata("LicenseURL", "https://opensource.org/licenses/MIT")]
 // The repository where your pluggin is hosted
-[assembly: AssemblyMetadata("Repository", "https://github.com/NirZonworked/NINA-TwoPointPolarAlignment")]
+[assembly: AssemblyMetadata("Repository", "https://github.com/nirzons/TwoPointPolarAlignment")]
 
 // The following attributes are optional for the official manifest meta data
 
 //[Optional] Your plugin homepage URL - omit if not applicaple
-[assembly: AssemblyMetadata("Homepage", "https://github.com/NirZonworked/NINA-TwoPointPolarAlignment")]
+[assembly: AssemblyMetadata("Homepage", "https://github.com/nirzons/TwoPointPolarAlignment")]
 
 //[Optional] Common tags that quickly describe your plugin
 [assembly: AssemblyMetadata("Tags", "")]
 
 //[Optional] A link that will show a log of all changes in between your plugin's versions
-[assembly: AssemblyMetadata("ChangelogURL", "https://github.com/NirZonworked/NINA-TwoPointPolarAlignment/blob/main/CHANGELOG.md")]
+[assembly: AssemblyMetadata("ChangelogURL", "https://github.com/nirzons/TwoPointPolarAlignment/blob/main/CHANGELOG.md")]
 
 //[Optional] The url to a featured logo that will be displayed in the plugin list next to the name
 [assembly: AssemblyMetadata("FeaturedImageURL", "")]
