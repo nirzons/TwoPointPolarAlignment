fcb7abb694bbeb51622346de91e22b87e214c099
Nir Zonshine <nirzon@gmail.com>
2026-05-26 10:36:43 +0300

    Bump version to v1.1.0.0 and add release manifest automation

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Runtime.InteropServices;
 
 // [MANDATORY] The assembly versioning
 //Should be incremented for each new release build of a plugin
-[assembly: AssemblyVersion("1.0.3.8")]
-[assembly: AssemblyFileVersion("1.0.3.8")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
 // [MANDATORY] The name of your plugin
 [assembly: AssemblyTitle("2-Point Polar Alignment")]
